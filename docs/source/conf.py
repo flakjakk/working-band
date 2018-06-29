aa730f0cb2cfa1267ffefdf476e68ed78b9f6c9c
Ed Kidgell <edkidgell@gmail.com>
2018-06-29 12:29:28 +0100

    Update conf.py
    Added Markdown support.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -164,3 +164,11 @@ texinfo_documents = [
 # Example configuration for intersphinx: refer to the Python standard library.
 intersphinx_mapping = {'https://docs.python.org/': None}
 
+from recommonmark.parser import CommonMarkParser
+
+source_parsers = {
+    '.md': CommonMarkParser,
+}
+
+source_suffix = ['.rst', '.md']
+
